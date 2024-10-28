7c0ad518faef380675a1b0e49cc3ede085c9fe7e
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2024-10-28 23:44:53 +0330

    add di

---
+++ Store.Api/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Store.Application;
+using Store.Infrastructure;
+
+namespace Store.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiDI(this IServiceCollection services)
+        {
+            services
+                .AddApplicationDI()
+                .AddInfrastructureDI();
+
+            return services;
+        }
+    }
+}
